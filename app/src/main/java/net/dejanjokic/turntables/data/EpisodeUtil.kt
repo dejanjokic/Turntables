26bd94349cd67bafbbfefa10cabb884d0bad39dc
Dejan Jokić <dejan.jokic7@gmail.com>
2020-07-03 02:50:10 +0200

    Updated random episode generator

--- app/src/main/java/net/dejanjokic/turntables/data/EpisodeUtil.kt
+++ app/src/main/java/net/dejanjokic/turntables/data/EpisodeUtil.kt
@@ -5,9 +5,12 @@ object EpisodeUtil {
     private val seasonEpisodeCounts = arrayOf(6, 22, 25, 19, 26, 26, 26, 24, 27)
 
     fun getRandomSeasonEpisodePair(): Pair<Int, Int> {
-        val season = (1..9).random()
-        val count = seasonEpisodeCounts[season-1]
-        val episode = (1..count).random()
-        return Pair(season, episode)
+        val seasonEpisodePairs = mutableListOf<Pair<Int, Int>>()
+        for (i in seasonEpisodeCounts.indices) {
+            for (j in 1..seasonEpisodeCounts[i]) {
+                seasonEpisodePairs.add(Pair(i+1, j))
+            }
+        }
+        return seasonEpisodePairs.random()
     }
 }
